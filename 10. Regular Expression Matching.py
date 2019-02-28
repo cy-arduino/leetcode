d9a4c79dd7803e4ec964b9a2b964e3eca356d47a
ChihYing <chihying.phone@gmail.com>
2019-02-28 20:17:51 +0800

    Update 10. Regular Expression Matching.py

--- 10. Regular Expression Matching.py
+++ 10. Regular Expression Matching.py
@@ -1,12 +1,5 @@
 from enum import Enum
 
-list_az=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-class exp_type(Enum):
-    NONE='none'
-    CHAR='char'
-    STAR='*'
-    
 class Solution:
     def isMatch(self, s: 'str', p: 'str') -> 'bool':
         #print('isMatch: s=', s , " p=", p)
