2b2f646af71ee43562842ec146872607590d5f56
ChihYing <chihying.phone@gmail.com>
2019-06-07 17:25:21 +0800

    Update 3. Longest Substring Without Repeating Characters.c

--- 3. Longest Substring Without Repeating Characters.c
+++ 3. Longest Substring Without Repeating Characters.c
@@ -1,42 +1,4 @@
-/*char bitmap[128/8];//128bit = 16byte. bitmap for ascii
-
-int exist(char c){
-    return (bitmap[c/8]&(1<<c%8)) > 0;
-}
-
-void put(char c){
-    bitmap[c/8] &= ~(1<<c%8);
-}
-
-void clear(){
-    memset((void*) bitmap, 0, 128/8);
-}
-
-int lengthOfLongestSubstring(char * s){
-    int ret = 0;
-        
-    for(int i = 0; i < strlen(s); i++){
-        clear();
-        int len = 0;
-        for (int j=i; j < strlen(s); j++){
-            if(exist(s[j])){
-                break;
-            }else{
-                put(s[j]);
-                //dump();
-                len++;
-            }
-        }
-        if(ret < len)
-            ret = len;
-    }
-    
-    clear();
-    
-    return ret;
-}*/
-
-char bitmap[128/8];//128bit = 16byte. bitmap for ascii
+char bitmap[128/8];//bitmap for ascii, 128bit = 16byte. 
 
 int exist(char c){
     return (bitmap[c/8]&(1<<c%8)) > 0;
@@ -54,6 +16,11 @@ void flush(){
     memset((void*) bitmap, 0, 128/8);
 }
 
+//i: substring's left idx
+//j: substring's right idx
+//ret: maxium of j-i
+//time complexity: O(n)
+//space: O(1) <== 128 bit for ascii
 int lengthOfLongestSubstring(char * s){
     int ret=0;
     int i=0,j=0;
@@ -72,6 +39,3 @@ int lengthOfLongestSubstring(char * s){
     
     return ret;
 }
-
-
-
