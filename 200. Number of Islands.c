06f56c81a94ae94bf1e707b70f6c04f20f9b60da
ChihYing <chihying.phone@gmail.com>
2019-06-21 13:30:08 +0800

    Update 200. Number of Islands.c

--- 200. Number of Islands.c
+++ 200. Number of Islands.c
@@ -1,3 +1,13 @@
+void dump(char** grid, int gridSize, int* gridColSize){
+    printf("\ndump:\n");
+    for(int row=0; row<gridSize; row++){
+        for(int col=0; col<gridColSize[row]; col++){
+            printf("%c ", grid[row][col]);
+        }
+        printf("\n");
+    }
+    
+}
 
 void extend(char** grid, int gridSize, int* gridColSize, int islandId, int row, int col){
     //printf("extend: row=%d, col=%d, islandId=%d\n", row, col, islandId);
@@ -28,6 +38,8 @@ int numIslands(char** grid, int gridSize, int* gridColSize){
             if(grid[row][col] == '1'){
                 extend(grid, gridSize, gridColSize, islandId, row, col);
                 islandId++;
+                
+                //dump(grid, gridSize, gridColSize);
             }
         }
     }
