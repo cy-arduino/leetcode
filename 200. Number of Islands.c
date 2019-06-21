515ed8d4319eca14e8dc69d33b3e6b4b54e20939
ChihYing <chihying.phone@gmail.com>
2019-06-21 13:26:39 +0800

    Create 200. Number of Islands.c

---
+++ 200. Number of Islands.c
@@ -0,0 +1,38 @@
+
+void extend(char** grid, int gridSize, int* gridColSize, int islandId, int row, int col){
+    //printf("extend: row=%d, col=%d, islandId=%d\n", row, col, islandId);
+    if(grid[row][col] == '1'){
+        grid[row][col] = 'a' + islandId;
+        
+        if(row-1 >=0){
+            extend(grid, gridSize, gridColSize, islandId, row-1, col);
+        }
+        if(row+1 < gridSize){
+            extend(grid, gridSize, gridColSize, islandId, row+1, col);
+        }
+        if(col-1 >=0){
+            extend(grid, gridSize, gridColSize, islandId, row, col-1);
+        }
+        if(col+1 < gridColSize[row]){
+            extend(grid, gridSize, gridColSize, islandId, row, col+1);
+        }
+    }
+}
+
+int numIslands(char** grid, int gridSize, int* gridColSize){
+
+    int islandId = 0;
+    
+    for(int row=0; row<gridSize; row++){
+        for(int col=0; col<gridColSize[row]; col++){
+            if(grid[row][col] == '1'){
+                extend(grid, gridSize, gridColSize, islandId, row, col);
+                islandId++;
+            }
+        }
+    }
+    
+    return islandId;
+
+}
+
