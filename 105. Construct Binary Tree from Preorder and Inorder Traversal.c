b970abf0defd5f9794c254a53219e3e98ab33166
ChihYing <chihying.phone@gmail.com>
2019-06-25 00:01:39 +0800

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.c

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.c
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.c
@@ -6,6 +6,44 @@
  *     struct TreeNode *right;
  * };
  */
+ 
+ 
+/*
+ My concept:
+ 
+ tree:
+				4
+		2				6
+	1		3		5		7
+	
+ preorder: 4213657
+ inorder : 1234567
+ 
+ #recursive 1
+ root = preorder[0] = 4
+ left
+	preorder: 213 <==same size of inorder
+	inorder : 123 <==left of root in inorder
+ right
+	preorder: 657
+	inorder : 567
+
+ #recursive 1.1
+ root = 2
+ left
+	preorder: 1
+	inorder : 1
+ right
+	preorder: 3
+	inorder : 3
+	
+ #recursive 1.1.1
+ root = 1
+ left
+	None
+ right
+	None
+*/
 
 void recursive(int* preorder, int preorderSize, int* inorder, int inorderSize, struct TreeNode **pRoot){
     if(!preorderSize)
