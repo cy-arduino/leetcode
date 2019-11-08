34080810eaf46f8e96244debe9835a4fc1f3ed99
ChihYing <chihying.phone@gmail.com>
2019-11-08 09:25:13 +0800

    Update 139. Word Break.c

--- 139. Word Break.c
+++ 139. Word Break.c
@@ -1,18 +1,18 @@
-int NOT_FOUND = 1;
-    
+//DFS
 bool match(char * s, int s_start, char ** wordDict, int wordDictSize, char* bitmap){    
+    //printf("s=%s, len=%d\n", s+s_start, strlen(s+s_start));   
     bool found = false;
-    if(bitmap[s_start] != NOT_FOUND){
+    if(!(bitmap[s_start/8] & (0x1 << s_start%8))){
         char* cur_s = s + s_start;
-        int s_len = strlen(cur_s);
+        int cur_s_len = strlen(cur_s);
 
         for(int i=0; i<wordDictSize; i++){
             char* word = wordDict[i];
             int word_len = strlen(word);
 
-            if(s_len >= word_len && !strncmp(cur_s, word, word_len)){
+            if(cur_s_len >= word_len && !strncmp(cur_s, word, word_len)){
                 //printf("  word[%d]=%s, len=%d\n", i, wordDict[i], word_len);        
-                if(s_len == word_len){
+                if(cur_s_len == word_len){
                     found = true;
                 }else{
                     found = match(s, s_start+word_len, wordDict, wordDictSize, bitmap);
@@ -21,18 +21,17 @@ bool match(char * s, int s_start, char ** wordDict, int wordDictSize, char* bitm
                     break;
             }
         }
-        
+
+        //DP, 1 means start from s_start is not found
         if(!found){
-            bitmap[s_start] = NOT_FOUND;
+            bitmap[s_start/8] |= (0x1 << (s_start%8));
         }
     }
     return found;
 }
 
 bool wordBreak(char * s, char ** wordDict, int wordDictSize){
-	//speedup, DP
-	//TODO: use real bit
-    char* bitmap = calloc(1000, sizeof(char));
+    char* bitmap = calloc(strlen(s)/8 + 1, sizeof(char));
     
     bool ret = match(s, 0, wordDict, wordDictSize, bitmap);
     
