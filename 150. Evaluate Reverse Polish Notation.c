a4363b08e4945b80cd15b14fc7c8741494e787ae
ChihYing <chihying.phone@gmail.com>
2019-12-29 23:51:40 +0800

    Update 150. Evaluate Reverse Polish Notation.c

--- 150. Evaluate Reverse Polish Notation.c
+++ 150. Evaluate Reverse Polish Notation.c
@@ -25,6 +25,7 @@ int pop(int *stack, int *stackSize){
 int evalRPN(char ** tokens, int tokensSize){
     int *stack = malloc(tokensSize * sizeof(int));
     int stackSize = 0;
+	int ret = 0;
     
     for(int i=0; i<tokensSize; i++){
         //printf("check tokens[%d]=%s, tokensSize=%d\n", i, tokens[i], tokensSize);
@@ -67,8 +68,11 @@ int evalRPN(char ** tokens, int tokensSize){
             push(stack, &stackSize, a, tokensSize);
         }
     }
-    
-    return pop(stack, &stackSize);
+    ret = pop(stack, &stackSize);
+	
+	free(stack);
+	
+    return ret;
 
 }
 
