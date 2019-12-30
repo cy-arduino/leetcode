76459e38653b113bdd530ae594c1707e94f442ed
ChihYing <chihying.phone@gmail.com>
2019-12-30 16:52:13 +0800

    Create 199. [array][dfs]Binary Tree Right Side View.c

---
+++ 199. [array][dfs]Binary Tree Right Side View.c
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/*
+time: O(n)
+space: avg. O{logn)
+Traverse all node and update it's level' value
+*/
+
+int maxRetSize = 100;
+
+
+void recursive(struct TreeNode *root, int **ret, int *returnSize, int level){
+    if(root){
+        
+        if(*returnSize < level+1){
+            *returnSize = level+1;
+            
+            if(*returnSize > maxRetSize){
+                maxRetSize *=2;
+                printf("Realloc: %d\n", maxRetSize);
+                *ret = realloc(*ret, maxRetSize * sizeof(int));
+            }
+        }
+        (*ret)[level] = root->val;
+        
+        //left
+        recursive(root->left, ret, returnSize, level+1);
+        
+        //right
+        recursive(root->right, ret, returnSize, level+1);
+    }
+}
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* rightSideView(struct TreeNode* root, int* returnSize){
+    
+    int *ret = calloc(maxRetSize, sizeof(int));
+    *returnSize = 0;
+    
+    recursive(root, &ret, returnSize, 0);    
+        
+    return ret;
+
+}
+
